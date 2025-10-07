42ed966b1b36ae6af1ffee343388cceebd32056f
TNC <89584074+Nghia251506@users.noreply.github.com>
2025-10-07 23:26:40 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -90,7 +90,8 @@ var allowedOrigins = new[]
     "https://www.chamkhoanhkhac.com",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://frontend-nghien-cf.vercel.app"
+    "https://frontend-nghien-cf.vercel.app",
+    "https://api.chamkhoanhkhac.com"
 };
 
 builder.Services.AddCors(opt =>
