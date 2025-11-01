cad46752304c7d587392f12ac82e4630bbda940f
Nghia251506 <ntn8530@gmail.com>
2025-11-01 18:43:29 +0700

    Delete image old

--- obj/Debug/net9.0/Backend_Nghiencf.AssemblyInfo.cs
+++ obj/Debug/net9.0/Backend_Nghiencf.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_Nghiencf")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd75a3b35a11bf528e4054e32305046ade738b3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+315906ae80662af64fbe811ea0dacd66c38021f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_Nghiencf")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_Nghiencf")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
